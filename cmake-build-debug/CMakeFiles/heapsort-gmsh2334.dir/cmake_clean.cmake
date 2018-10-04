643b2661a475021c6dc3b699955475e72138cbe6
gonzalo <gonzalo.munoz@utec.edu.pe>
2018-10-04 15:27:40 -0500

    finished

---
+++ cmake-build-debug/CMakeFiles/heapsort-gmsh2334.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/heapsort-gmsh2334.dir/main.cpp.obj"
+  "heapsort-gmsh2334.pdb"
+  "heapsort-gmsh2334.exe"
+  "heapsort-gmsh2334.exe.manifest"
+  "heapsort-gmsh2334.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/heapsort-gmsh2334.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <random>
-#include <assert.h>
-#include <string.h>
+#include <cassert>
+#include <cstring>
+#include <iterator>
+#include <vector>
 
 using namespace std;
 
@@ -13,7 +15,7 @@ mt19937 rng;
 int generateRandomInt(int min, int max);
 void printArray(int *array, size_t size);
 void heapsort(int* array, size_t size);
-bool validate(int* array, size_t size);
+bool validate(const int* array, size_t size);
 
 int main(int argc, char *argv[]) {
     rng.seed(random_device()());
@@ -22,37 +24,69 @@ int main(int argc, char *argv[]) {
     cout << "===========================================================" << endl << endl;
 
     const int numberOfElements = generateRandomInt(MIN, MAX);
-    int *numbers = new int[numberOfElements];
+    auto *numbers = new int[numberOfElements];
     for (int i = 0; i < numberOfElements; i++) {
         numbers[i] = generateRandomInt(0, 100);
     }
 
-    printArray(numbers, numberOfElements);
-    heapsort(numbers, numberOfElements);
-    printArray(numbers, numberOfElements);
-    assert(validate(numbers, numberOfElements) && "The sort is not ordering all the elements");
-
-    system("read");
+    printArray(numbers, (size_t) numberOfElements);
+    heapsort(numbers, (size_t) numberOfElements);
+    printArray(numbers, (size_t) numberOfElements);
+    assert(validate(numbers, (size_t) numberOfElements) && "The sort is not ordering all the elements");
+    cout<<"Presione una tecla...";
+    cin.get();
     return EXIT_SUCCESS;
 }
 
 int generateRandomInt(int min, int max) {
-    uniform_int_distribution<mt19937::result_type> distribution(min, max); 
+    uniform_int_distribution<mt19937::result_type> distribution(static_cast<unsigned int>(min),
+                                                                static_cast<unsigned int>(max));
     return distribution(rng);
 }
 
+
 void printArray(int *array, size_t size) {
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
         cout << array[i] << " ";
+    cout << "\n\n";
+}
+
+
+
+
+void heap(int* array, size_t i, size_t delim)
+{
+    size_t left=2*i+1, right=2*i+2;
+
+    if (right >= delim){
+        return;}
+
+    else if ((array[left] > array[right]) && (array[left] > array[i])) {
+        swap(array[i], array[left]);
+        heap(array, left, delim);
     }
-    cout << endl;
+
+    else if(array[right] > array[i]) {
+        swap(array[i], array[right]);
+        heap(array, right, delim);
+    }
 }
 
 void heapsort(int* array, size_t size) {
+
+    for (int i=(size/2)-1; i>=0; --i){
+        heap(array, static_cast<size_t>(i), size - 1);
+    }
+
+    for (int i=size-1; i>0; --i) {
+        swap(array[i], array[0]);
+        heap(array, 0, static_cast<size_t>(i));
+    }
+
     // TODO
 }
 
-bool validate(int* array, size_t size) {
+bool validate(const int* array, size_t size) {
     for (int i = 0; i < size - 1; i++) {
         if (array[i] > array[i + 1]) {
             return false;
